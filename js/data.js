69e93dfdb9601ea8a92e3ec65a24ba2990694387
Garrett Quathamer <gquathamer@gmail.com>
2022-03-31 20:50:19 -0700

    completed issue 1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,9 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousDataModelString = localStorage.getItem('data-model');
+
+if (previousDataModelString !== null) {
+  data = JSON.parse(previousDataModelString);
+}
